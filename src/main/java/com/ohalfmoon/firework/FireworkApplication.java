61a9ffa2b1e8884d35989038702646689e84792e
OhHalfmoon <sangoh7395@gmail.com>
2023-05-31 13:14:27 +0900

    최초 커밋

---
+++ src/main/java/com/ohalfmoon/firework/FireworkApplication.java
@@ -0,0 +1,13 @@
+package com.ohalfmoon.firework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireworkApplication.class, args);
+	}
+
+}
