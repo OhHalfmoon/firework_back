7d5a9921ac972e55190e950b5d71899393e81f7b
cksdydsla93@gmail.com <cksdydsla93@gmail.com>
2023-06-06 01:25:52 +0900

    1. 회원가입 기능 구현  1-1. pk값이 String일 경우 500에러 발생(id가 수동으로 할당되어야 함)    - tbl_member2 table 생성, 기존 테이블에서 id라는 새로운 컬럼을 생성하였고, pk지정, username은 uk로 설정    - 이후 값이 잘 들어가는것 확인  1-2. birthdate, startdate, enddate dateformat 설정  1-3. fk값 받아오는 방법 찾는중 (후순위)
    2. 로그인
     - 기능구현중 (security 적용예정)
    
    3. 결론
      3-1. 1-1에러로 인하여 시간을 많이 소요하였고 회원가입은 password 암호화까지만 하고 우선순위에 있는 다른 작업을 먼저 하고 작업하겠음
      3-2. 1-1 에러로그
    
      This application has no explicit mapping for /error, so you are seeing this as a fallback.
    
      Sun Jun 04 23:36:03 KST 2023
      There was an unexpected error (type=Internal Server Error, status=500).
      ids for this class must be manually assigned before calling save(): com.ohalfmoon.firework.model.MemberEntity; nested exception is org.hibernate.id.IdentifierGenerationException: ids for this class must be manually assigned before calling save(): com.ohalfmoon.firework.model.MemberEntity

--- src/main/java/com/ohalfmoon/firework/controller/MemberController.java
+++ src/main/java/com/ohalfmoon/firework/controller/MemberController.java
@@ -1,15 +1,13 @@
 package com.ohalfmoon.firework.controller;
 
 import com.ohalfmoon.firework.dto.MemberDTO;
+import com.ohalfmoon.firework.model.DeptEntity;
 import com.ohalfmoon.firework.model.MemberEntity;
 import com.ohalfmoon.firework.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("auth")
@@ -23,7 +21,29 @@ public class MemberController {
 
     @PostMapping("signup")
     public String register(MemberDTO memberDTO) {
-        memberService.register(memberDTO.toEntity());
+        MemberEntity memberEntity = MemberEntity.builder()
+                .username(memberDTO.getUsername())
+                .password(memberDTO.getPassword())
+                .email(memberDTO.getEmail())
+                .phoneNum(memberDTO.getPhoneNum())
+                .name(memberDTO.getName())
+                .startdate(memberDTO.getStartdate())
+                .birthdate(memberDTO.getBirthdate())
+                .startdate(memberDTO.getStartdate())
+//                .deptEntity(memberDTO.getDeptNo())
+                .build();
+        MemberEntity registerMember =  memberService.register(memberEntity);
+        MemberDTO responseDTO = MemberDTO.builder()
+                .username(registerMember.getUsername())
+                .build();
+        return "redirect:/login";
+    }
+
+    @GetMapping("signin")
+    public void login() {}
+
+    @PostMapping("signin")
+    public String login(MemberDTO memberDTO) {
         return "redirect:/";
     }
 }
--- src/main/java/com/ohalfmoon/firework/persistence/MemberRepository.java
+++ src/main/java/com/ohalfmoon/firework/persistence/MemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.lang.reflect.Member;
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<MemberEntity, String> {
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 }
