5d15e782856278d6f79e8e5712cb1b21da92daaa
OhHalfmoon <sangoh7395@gmail.com>
2023-06-07 12:02:29 +0900

    approvalEntity 생성

---
+++ src/main/java/com/ohalfmoon/firework/model/DocboxEntity.java
@@ -0,0 +1,49 @@
+package com.ohalfmoon.firework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * packageName    : com.ohalfmoon.firework.model
+ * fileName       : DocboxEntity
+ * author         : 오상현
+ * date           : 2023/06/07
+ * description    : 문서함 Entity
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/06/07        오상현           최초 생성
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tbl_docbox")
+@DynamicInsert
+@Builder
+public class DocboxEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long docboxNo;
+
+    @Column(nullable = false)
+    private String docboxName;
+
+    private Date regdate;
+
+    private Date updatedate;
+
+    @Builder
+    public DocboxEntity(
+            String docboxName
+    ) {
+        this.docboxName = docboxName;
+    }
+
+}
