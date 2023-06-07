e3a7ba9667d79b6c17e8588d815928ddb234c48a
joon <yijiyoon06@gmail.com>
2023-06-07 18:20:23 +0900

    Master, Sub Repository 주석추가

--- src/main/java/com/ohalfmoon/firework/persistence/MasterLineRepository.java
+++ src/main/java/com/ohalfmoon/firework/persistence/MasterLineRepository.java
@@ -4,6 +4,18 @@ import com.ohalfmoon.firework.model.MasterLineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * packageName    : com.ohalfmoon.firework.persistence
+ * fileName       : MasterLineRepository
+ * author         : 이지윤
+ * date           : 2023/06/07
+ * description    : 기본적인 CRUD 작업
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/06/07        이지윤           최초 생성
+ */
+
 @Repository
 public interface MasterLineRepository extends JpaRepository<MasterLineEntity, Long> {
 }
--- src/main/java/com/ohalfmoon/firework/persistence/SubLineRepository.java
+++ src/main/java/com/ohalfmoon/firework/persistence/SubLineRepository.java
@@ -4,6 +4,17 @@ import com.ohalfmoon.firework.model.SubLineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * packageName    : com.ohalfmoon.firework.persistence
+ * fileName       : SubLineRepository
+ * author         : 이지윤
+ * date           : 2023/06/07
+ * description    : 기본적인 CRUD 작업
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/06/07        이지윤           최초 생성
+ */
 @Repository
 public interface SubLineRepository extends JpaRepository<SubLineEntity, Long> {
 }
