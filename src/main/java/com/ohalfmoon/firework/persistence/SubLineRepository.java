c3d4568b8a9b03a72f7940327df06bbe89384393
joon <yijiyoon06@gmail.com>
2023-06-08 17:42:58 +0900

    서브라인 레파지토리 리스트 조회 추가

--- src/main/java/com/ohalfmoon/firework/persistence/SubLineRepository.java
+++ src/main/java/com/ohalfmoon/firework/persistence/SubLineRepository.java
@@ -1,8 +1,11 @@
 package com.ohalfmoon.firework.persistence;
 
+import com.ohalfmoon.firework.model.MasterLineEntity;
 import com.ohalfmoon.firework.model.SubLineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * packageName    : com.ohalfmoon.firework.persistence
@@ -17,4 +20,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface SubLineRepository extends JpaRepository<SubLineEntity, Long> {
+    List<SubLineEntity> findAllByLineNo(MasterLineEntity lineNo);
 }
