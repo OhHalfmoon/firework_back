81cdd9bb455cf243857686f2d2cec0ac83dad549
joon <yijiyoon06@gmail.com>
2023-06-12 00:50:48 +0900

    마스터, 서브 라인 레파지토리 리스트 조회 형식에 맞게 수정

--- src/main/java/com/ohalfmoon/firework/persistence/SubLineRepository.java
+++ src/main/java/com/ohalfmoon/firework/persistence/SubLineRepository.java
@@ -3,6 +3,7 @@ package com.ohalfmoon.firework.persistence;
 import com.ohalfmoon.firework.model.MasterLineEntity;
 import com.ohalfmoon.firework.model.SubLineEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,14 +13,19 @@ import java.util.List;
  * fileName       : SubLineRepository
  * author         : 이지윤
  * date           : 2023/06/07
- * description    : 기본적인 CRUD 작업
+ * description    : 기본적인 CRUD 작업, 리스트 조회
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2023/06/07        이지윤           최초 생성
+ * 2023/06/11        이지윤           리스트 조회 수정
  */
 @Repository
 public interface SubLineRepository extends JpaRepository<SubLineEntity, Long> {
 //    List<SubLineEntity> findAllByLineNo(MasterLineEntity lineNo);
 //    List<SubLineEntity> findAllByLineNo();
+
+     // using LineNo to find SubLine
+     List<SubLineEntity> findAllByMasterLineEntity_LineNo(Long lineNo);
+
 }
