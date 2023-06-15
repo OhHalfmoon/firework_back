562fcbc4bd23a46aa1c6c3554fddba143c0d0d4f
joon <yijiyoon06@gmail.com>
2023-06-15 18:26:26 +0900

    출근 테스트 생성

--- src/test/java/com/ohalfmoon/firework/service/AttendServiceTests.java
+++ src/test/java/com/ohalfmoon/firework/service/AttendServiceTests.java
@@ -1,2 +1,35 @@
-package com.ohalfmoon.firework.service;public class AttendServiceTests {
+package com.ohalfmoon.firework.service;
+
+import com.ohalfmoon.firework.dto.attend.AttendSaveDTO;
+import com.ohalfmoon.firework.dto.attend.AttendSaveLeaveWorkDTO;
+import com.ohalfmoon.firework.model.AttendEntity;
+import com.ohalfmoon.firework.persistence.AttendRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+@SpringBootTest
+@Slf4j
+public class AttendServiceTests {
+    @Autowired
+    private AttendRepository attendRepository;
+
+    @Test
+    @DisplayName("출근 등록 테스트")
+    public void testSave() {
+        AttendEntity attendEntity = attendRepository.save(AttendSaveDTO.builder()
+                .godate(new Date())
+                .build().toEntity());
+    }
+    @Test
+    @DisplayName("퇴근 등록(수정) 테스트")
+    public void testUpdate() {
+        AttendEntity attendEntity = AttendSaveLeaveWorkDTO.builder()
+                .leavedate(new Date())
+                .build().toEntity();
+    }
 }
