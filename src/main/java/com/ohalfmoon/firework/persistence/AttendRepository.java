233f3ab786087e4455180b6931669d188a25ca54
joon <yijiyoon06@gmail.com>
2023-06-15 18:20:48 +0900

    근태 레파지토리 생성

--- src/main/java/com/ohalfmoon/firework/persistence/AttendRepository.java
+++ src/main/java/com/ohalfmoon/firework/persistence/AttendRepository.java
@@ -3,6 +3,16 @@ package com.ohalfmoon.firework.persistence;
 
 import com.ohalfmoon.firework.model.AttendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * packageName    : com.ohalfmoon.firework.persistence
+ * fileName       : AttendRepository
+ * author         : 이지윤
+ * date           : 2023/06/15
+ * description    : 근태 Repository
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/06/15        이지윤           최초 생성
+ */
 public interface AttendRepository extends JpaRepository<AttendEntity, Long> {
 }
