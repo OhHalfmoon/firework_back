61bd754fea7b107ef7133d9d7d9d48ed3d9e730d
joon <yijiyoon06@gmail.com>
2023-06-19 09:27:37 +0900

    출퇴근 아이콘 추가

--- src/main/java/com/ohalfmoon/firework/controller/AttendController.java
+++ src/main/java/com/ohalfmoon/firework/controller/AttendController.java
@@ -1,16 +1,38 @@
 package com.ohalfmoon.firework.controller;
 
+import com.ohalfmoon.firework.config.auth.CustomUserDetails;
+import com.ohalfmoon.firework.dto.attend.AttendSaveDTO;
+import com.ohalfmoon.firework.dto.attend.AttendUpdateDTO;
 import com.ohalfmoon.firework.service.AttendService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("attend")
+@Slf4j
 public class AttendController {
     @Autowired
     private AttendService attendService;
 
+    @PostMapping("/{userNo}")
+    public void gotoWork(@AuthenticationPrincipal CustomUserDetails user, @RequestBody AttendSaveDTO attendSaveDTO) {
+        attendSaveDTO.setUserNo(user.getUserNo());
+        attendService.save(attendSaveDTO);
+    }
 
-
-
-
+    @PutMapping("/{attendNo}")
+    public void leaveWork(@RequestBody AttendUpdateDTO attendUpdateDTO, @PathVariable Long attendNo) {
+        // attendNo, leavedate
+        log.info("attendNo : {}", attendNo);
+        attendService.updateAttend(attendNo, attendUpdateDTO);
+    }
 }
+
+
+
