ab935f02f7b9dec2cfd5622d7c4c50dbe8c50653
joon <yijiyoon06@gmail.com>
2023-06-27 10:01:23 +0900

    첨부파일명 필드  추가a

--- src/main/java/com/ohalfmoon/firework/dto/board/BoardResponseDTO.java
+++ src/main/java/com/ohalfmoon/firework/dto/board/BoardResponseDTO.java
@@ -5,18 +5,21 @@ import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * packageName    : com.ohalfmoon.firework.dto.board
  * fileName       : BoardResponseDTO
  * author         : 이지윤
  * date           : 2023/06/22
- * description    : 게시판 조회 시 필요한 정보 DTO
+ * description    : 게시판 조회 시 필요한 정보 DTO, 첨부파일 이름
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2023/06/22        이지윤           최초 생성
  * 2023/06/23        이지윤           boardNo 추가
+ * 2023/06/26        이지윤           originName 추가
  */
 
 @Data
@@ -30,12 +33,16 @@ public class BoardResponseDTO {
     private String content;
     private Long userNo;
     private Date regDate;
+    private List<String> originName;
 
     public BoardResponseDTO(final BoardEntity boardEntity) {
         boardNo = boardEntity.getBoardNo();
         title = boardEntity.getBoardTitle();
         content = boardEntity.getBoardContent();
         regDate = boardEntity.getRegdate();
+        originName = boardEntity.getAttachEntities()
+                .stream().map(attachEntity -> attachEntity.getOriginName())
+                .collect(Collectors.toList());
     }
 
     public BoardEntity toEntity() {
