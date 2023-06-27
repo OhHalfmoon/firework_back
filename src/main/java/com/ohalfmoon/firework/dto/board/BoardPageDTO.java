4be485a6cbd66e434e07cd151539ae8a99241a88
joon <yijiyoon06@gmail.com>
2023-06-27 10:00:02 +0900

    페이징 처리를 위한  DTO

--- src/main/java/com/ohalfmoon/firework/dto/board/BoardPageDTO.java
+++ src/main/java/com/ohalfmoon/firework/dto/board/BoardPageDTO.java
@@ -1,2 +1,28 @@
-package com.ohalfmoon.firework.dto.board;public class BoardPageDTO {
+package com.ohalfmoon.firework.dto.board;
+
+import com.ohalfmoon.firework.dto.paging.PageResponseDTO;
+import lombok.*;
+import org.springframework.data.domain.Page;
+
+/**
+ * packageName    : com.ohalfmoon.firework.dto.board
+ * fileName       : BoardPageDto
+ * author         : 이지윤
+ * date           : 2023/06/26
+ * description    : 게시판 리스트 DTO
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/06/26        이지윤           최초 생성
+ */
+
+@Getter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class BoardPageDTO {
+    private PageResponseDTO pageResponseDTO;
+
+    private Page<BoardResponseDTO> boardList;
 }
