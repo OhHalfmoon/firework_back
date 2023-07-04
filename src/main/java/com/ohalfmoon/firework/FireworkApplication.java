788783d594f126fe72c37f8a29281baed7bc0835
ycy <cksdyd93@gmail.com>
2023-07-04 12:04:45 +0900

    war 설정파일 푸시

--- src/main/java/com/ohalfmoon/firework/FireworkApplication.java
+++ src/main/java/com/ohalfmoon/firework/FireworkApplication.java
@@ -1,15 +1,15 @@
-package com.ohalfmoon.firework;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@SpringBootApplication
-public class FireworkApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FireworkApplication.class, args);
-	}
-
-}
+//package com.ohalfmoon.firework;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+//@EnableJpaAuditing
+//@SpringBootApplication
+//public class FireworkApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(FireworkApplication.class, args);
+//	}
+//
+//}
--- src/main/java/com/ohalfmoon/firework/ServletInitializer.java
+++ src/main/java/com/ohalfmoon/firework/ServletInitializer.java
@@ -1,13 +1,18 @@
 package com.ohalfmoon.firework;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
-
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FireworkApplication.class);
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(ServletInitializer.class, args);
+	}
 }
